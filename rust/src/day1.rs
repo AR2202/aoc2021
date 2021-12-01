308baf72d880b2f9a2b25f8ccc695e80cf35b235
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-12-01 22:01:00 +0000

    added Day1 Part 2 solution in Rust

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,4 +1,4 @@
-pub mod day1a {
+pub mod day1sol {
     use std::fs;
 
     pub fn day1a_sol(filename: &str) -> i32 {
@@ -6,7 +6,7 @@ pub mod day1a {
         let mut numbers = contents.lines().map(|line| line.parse::<i32>().unwrap());
         let mut prev = numbers.next().unwrap();
 
-        let n_increase = numbers.fold(0, |acc, num| {
+        numbers.fold(0, |acc, num| {
             if num > prev {
                 prev = num;
                 acc + 1
@@ -14,11 +14,21 @@ pub mod day1a {
                 prev = num;
                 acc
             }
-        });
-
-        n_increase
+        })
     }
+    pub fn day1b_sol(filename: &str) -> i32 {
+        let contents = fs::read_to_string(filename).unwrap();
+        let numbers: Vec<i32> = contents
+            .lines()
+            .map(|line| line.parse::<i32>().unwrap())
+            .collect();
+        let dropped3 = &numbers[3..];
 
+        numbers
+            .iter()
+            .zip(dropped3.iter())
+            .fold(0, |acc, (x, y)| if y > x { acc + 1 } else { acc })
+    }
     // Test for day1 part1
     #[cfg(test)]
     mod tests {
@@ -27,6 +37,11 @@ pub mod day1a {
         #[test]
         fn day1a_test() {
             assert_eq!(day1a_sol("../input/example1.txt"), 7);
+        }
+
+        #[test]
+        fn day1b_test() {
+            assert_eq!(day1b_sol("../input/example1.txt"), 5);
         }
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
 mod day1;
-pub use crate::day1::day1a;
+pub use crate::day1::day1sol;
 
 fn main() {
-    let day1 = day1a::day1a_sol("../input/day1.txt");
-    println!("Solution to day1: {}", day1);
+    let day1a = day1sol::day1a_sol("../input/day1.txt");
+    println!("Solution to day1 part 1: {}", day1a);
+    let day1b = day1sol::day1b_sol("../input/day1.txt");
+    println!("Solution to day1 part 2: {}", day1b);
 }
