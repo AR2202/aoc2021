3b21e5a8d3eaabe0fca5f0f0043c470961f78242
Annika Rings <annika.rings@cncb.ox.ac.uk>
2021-12-03 20:23:53 +0000

    added Day3 part 2

--- haskell/src/Day3.hs
+++ haskell/src/Day3.hs
@@ -1,6 +1,8 @@
 module Day3
   ( example3a
   , day3a
+  , example3b
+  , day3b
   ) where
 
 import           Common
@@ -23,9 +25,10 @@ solutionDay3a file = do
   print $ string2decimal mostCommon * string2decimal leastCommon
 
 
+
 mostCommonBit bitstates
-  |ones > zeros = '1'
-  |otherwise = '0'
+  |zeros > ones = '0'
+  |otherwise = '1'
     where ones = length .  filter (== '1') $ bitstates
           zeros = length .  filter (== '0')$ bitstates 
 
@@ -39,3 +42,33 @@ string2decimal string =
 
 readChar2digit '1' = 1
 readChar2digit '0' = 0
+
+-------------------
+-- day 3 part 2
+-------------------
+example3b = solutionDay3b "../input/example3.txt"
+day3b = solutionDay3b "../input/day3.txt"
+
+solutionDay3b file = do
+  input <- loadAndSplitLines file
+  
+  let ox =  oxygenRating input
+  let co2 = co2Rating input
+  print $ string2decimal ox
+  print $ string2decimal co2
+  
+  print $ string2decimal ox * string2decimal co2
+
+keepMostCommon bitlist = filter (\x -> head x == mostCommon bitlist) bitlist
+
+mostCommon  bitlist = mostCommonBit $ map head bitlist
+
+keepLeastCommon bitlist = filter (\x -> head x == leastCommon bitlist) bitlist
+leastCommon bitlist = invertState $ mostCommon bitlist
+
+rating handler1 handler2 bitlist = go bitlist ""
+  where go [x] ys =  reverse ys ++ x
+        go xs ys = go (map tail $ handler1 xs) (handler2 xs : ys)
+
+oxygenRating  = rating keepMostCommon mostCommon
+co2Rating = rating keepLeastCommon leastCommon
