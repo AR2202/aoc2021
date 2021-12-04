054c13a56a2107ddb6d573ddfc4480a6a2af6d6c
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-12-04 20:32:39 +0000

    added Day4 part 2

--- haskell/src/Day4.hs
+++ haskell/src/Day4.hs
@@ -1,5 +1,7 @@
 module Day4
   ( day4a
+  , day4b
+  , example4b
   ) where
 
 import           Common
@@ -9,13 +11,13 @@ import           Data.Maybe (fromMaybe)
 -----------------------
 -- reading Input
 -----------------------
-readBoards filename = do
+readBoards solver filename = do
   input <- splitOnBlankLine filename
   let drawn = map read $ splitOnComma $ head input
   let boards =
         map (listToBingoBoard . map filterOutSpaces . splitOnSpace . lines) $
         tail input
-  let winning = playBingo boards drawn
+  let winning = solver boards drawn
   let score = countRemaining $ head $ fst winning
   let lastNumber = fromMaybe 0 $ snd winning
   let answer = score * lastNumber
@@ -34,7 +36,7 @@ data BingoBoard =
 ----------------------
 -- Part 1
 ----------------------
-day4a = readBoards "../input/day4.txt"
+day4a = readBoards playBingo "../input/day4.txt"
 
 filterOutSpaces = filter (/= "")
 
@@ -43,8 +45,8 @@ listToBingoBoard list =
 
 isWinning board = completeRow || completeColumn
   where
-    completeRow = any (== []) $ rows board
-    completeColumn = any (== []) $ columns board
+    completeRow = elem [] $ rows board
+    completeColumn = elem [] $ columns board
 
 crossOut number board = BingoBoard newrows newcolumns
   where
@@ -58,5 +60,21 @@ playBingo boards (x:xs)
   | winningBoards /= [] = (winningBoards, Just x)
   | otherwise = playBingo newboardstates xs
   where
+    winningBoards = filter isWinning newboardstates
+    newboardstates = map (crossOut x) boards
+
+--------------------
+-- Part 2
+--------------------
+losingBingo boards [] = ([], Nothing)
+losingBingo boards (x:xs)
+  | (length boards == 1) && (winningBoards /= []) = (winningBoards, Just x)
+  | otherwise = losingBingo removedWinning xs
+  where
+    removedWinning = filter (not . isWinning) newboardstates
     newboardstates = map (crossOut x) boards
     winningBoards = filter isWinning newboardstates
+
+day4b = readBoards losingBingo "../input/day4.txt"
+
+example4b = readBoards losingBingo "../input/example4.txt"
